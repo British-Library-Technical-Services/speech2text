d55a86d354fedaf618eec72505b2679744935f8a
realgoodegg <tomisalive@gmail.com>
2024-07-04 13:50:25 +0100

    refactor: Refactor whisper_hf_pipeline.py for better structure and readability

--- whisper_hf_pipeline.py
+++ whisper_hf_pipeline.py
@@ -1,21 +1,29 @@
+
+from dotenv import load_dotenv
+import glob
 import torch
 from transformers import pipeline
 import os
 import json
 
-model = ""
+MODEL = os.getenv("MODEL_TINY")
+AUDIO_LOCATION = os.getenv("AUDIO_LOCATION")
+JSON_OUTPUT = os.getenv("JSON_OUTPUT")
 
-pipe = pipeline(
-    "automatic-speech-recognition", model=model, chunk_length_s=20, device="cpu"
-)
+FILE_LIST = glob.glob(AUDIO_LOCATION + "/*.wav")
 
-audio = ""
+def transcribe_speech(audio_file):
+    pipe = pipeline(
+        "automatic-speech-recognition", model=MODEL, chunk_length_s=20, device="cpu"
+    )
 
-prediction = pipe(audio, batch_size=8, return_timestamps=True)["chunks"]
+    prediction = pipe(audio_file, batch_size=8, return_timestamps=True)["chunks"]
+    output = json.dumps(prediction, indent=2)
 
-filename = os.path.basename(audio)
+    filename = os.path.basename(audio_file).replace(".wav", ".json")
 
-output = json.dumps(prediction, indent=2)
+    with open(os.path.join(JSON_OUTPUT, filename), "w") as f:
+        f.write(output)
 
-with open(filename, "w") as f:
-    f.write(output)
+for file in FILE_LIST:
+    transcribe_speech(file)
