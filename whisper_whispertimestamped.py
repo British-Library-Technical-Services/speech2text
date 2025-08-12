35949f82b9b4c9c22da70f192245a1bb8196f433
realgoodegg <tomisalive@gmail.com>
2025-08-12 19:04:39 +0100

    refactor: Modify transcribe_speech function to accept MODEL and JSON_OUTPUT parameters, and update whispertimestamped_speech2text_pipeline accordingly

--- whisper_whispertimestamped.py
+++ whisper_whispertimestamped.py
@@ -1,23 +1,18 @@
 import os
 import whisper_timestamped as whisper
-import glob
 import json
 
-MODEL = os.getenv("PT_MODEL_TINY")
-AUDIO_LOCATION = os.getenv("AUDIO_LOCATION")
-JSON_OUTPUT = os.getenv("JSON_OUTPUT")
-
-FILE_LIST = glob.glob(AUDIO_LOCATION + "/*.wav")
-
-def transcribe_speech(audio_file):
+def transcribe_speech(audio_file, MODEL, JSON_OUTPUT):
     load_model = whisper.load_model(MODEL)
     load_audio = whisper.load_audio(audio_file)
 
+    output = None  # Initialize output
     try:
         transcribe = whisper.transcribe(load_model, load_audio, language="en")
         output = json.dumps(transcribe, indent=2)
     except Exception as e:
         print(f"Prediction Error: {e}")
+        return  # Return early if transcription fails
     
     filename = os.path.basename(audio_file).replace(".wav", "")
 
@@ -27,5 +22,6 @@ def transcribe_speech(audio_file):
     except IOError as e:
         print(f"File Write Error: {e}")
 
-for file in FILE_LIST:
-    transcribe_speech(file)
+def whispertimestamped_speech2text_pipeline(FILE_LIST, MODEL, JSON_OUTPUT):
+    for file in FILE_LIST:
+        transcribe_speech(file, MODEL, JSON_OUTPUT)
